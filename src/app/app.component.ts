f4aacbbdc192b6fe8883f983cc068f3c44e26934
Roman_C <mymind7@gmail.com>
2022-04-01 09:33:55 +0300

    dispatch close card window state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
-import { filter, map, skipWhile } from 'rxjs';
+import { filter, map, merge, skipWhile } from 'rxjs';
 import { closeCardInput } from './components/card/card-input.component/state/card-input.actions';
 import { CardDialogComponent } from './components/card/card.component/card.component';
-import { getCardsList } from './components/card/card.component/state/card.actions';
+import { cloeCardWindow, getCardsList } from './components/card/card.component/state/card.actions';
 import { selectCards, selectCardWindow } from './components/card/card.component/state/card.selectors';
 import { cardWindowState } from './components/card/card.component/state/card.window.reducers';
 import { Card } from './models/card.model';
@@ -17,7 +17,7 @@ import { ClickService } from './services/click-service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  cards$ = this.store.select(selectCards);
+  cards$  = this.store.select(selectCards);
   window$ = this.store.select(selectCardWindow);
   
   constructor(
@@ -28,19 +28,32 @@ export class AppComponent {
                                                                                                   ) {}
 
   ngOnInit() {
-    const that = this;
     this.cardList.getCards().subscribe(cards => this.store.dispatch(getCardsList({cards})));
-    this.window$.pipe(
-        filter( (state: cardWindowState) => state.isOpen != false),
-        map(state => state.card as Card)
-      ).subscribe(card => that.openCardWindow(card));
+    this.subscribeToWindowState();
   }
 
   click(event: MouseEvent) {
     this.store.dispatch(closeCardInput());
   }
 
-  openCardWindow(card: Card) {
-    this.modalDialog.open(CardDialogComponent, {data: {card}, position: {top: '100px'}});
+  private openCardWindow(card: Card) {
+    const dialog = this.modalDialog.open(CardDialogComponent, 
+      {data: {card}, position: {top: '100px'}, disableClose: true});
+
+    const clicks = dialog.backdropClick();
+    const keybordEvents = dialog.keydownEvents().pipe(filter(({key}) => key === 'Escape'));
+    merge(clicks, keybordEvents)
+      .subscribe(() => this.store.dispatch(cloeCardWindow()))
+  }
+
+  private subscribeToWindowState() {
+    this.window$.pipe(
+      filter( (state: cardWindowState) => state.isOpen != false),
+      map(state => state.card as Card)
+    ).subscribe(card => this.openCardWindow(card));
+
+    this.window$.pipe(
+      filter( (state: cardWindowState) => state.isOpen == false)
+    ).subscribe(_ => this.modalDialog.closeAll())
   }
 }
